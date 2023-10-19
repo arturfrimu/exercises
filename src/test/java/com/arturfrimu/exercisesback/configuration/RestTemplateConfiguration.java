f695f4140816828d11a6e8d217d1233ecc276c13
Artur Frimu <artur.frimu2@gmail.com>
2023-10-19 19:31:21 +0300

    @Bean public RestTemplate restTemplate()

--- src/test/java/com/arturfrimu/exercisesback/configuration/RestTemplateConfiguration.java
+++ src/test/java/com/arturfrimu/exercisesback/configuration/RestTemplateConfiguration.java
@@ -3,6 +3,7 @@ package com.arturfrimu.exercisesback.configuration;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class RestTemplateConfiguration {
@@ -10,5 +11,10 @@ public class RestTemplateConfiguration {
     @Bean
     public TestRestTemplate testRestTemplate() {
         return new TestRestTemplate();
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
