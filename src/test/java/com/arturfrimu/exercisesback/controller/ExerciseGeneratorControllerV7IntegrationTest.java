85ed638f4e380caa3198971f66836ee8b90c18e9
Artur Frimu <artur.frimu2@gmail.com>
2023-12-07 20:37:17 +0200

    Add new integration test  using @WithMockUser  .andExpect(content().json(expectedJson));

--- src/test/java/com/arturfrimu/exercisesback/controller/ExerciseGeneratorControllerV7IntegrationTest.java
+++ src/test/java/com/arturfrimu/exercisesback/controller/ExerciseGeneratorControllerV7IntegrationTest.java
@@ -4,6 +4,7 @@ import com.arturfrimu.exercisesback.ExercisesBackApplication;
 import com.arturfrimu.exercisesback.controller.response.ExerciseResponse;
 import com.arturfrimu.exercisesback.enumeration.Status;
 import com.arturfrimu.exercisesback.service.GenerateExerciseService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,6 +28,8 @@ class ExerciseGeneratorControllerV7IntegrationTest {
 
     @Autowired
     private WebApplicationContext webApplicationContext;
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @MockBean
     private GenerateExerciseService generateExerciseService;
@@ -55,5 +58,24 @@ class ExerciseGeneratorControllerV7IntegrationTest {
                 .andExpect(jsonPath("$.expression").value(mockResponse.expression()))
                 .andExpect(jsonPath("$.status").value(mockResponse.status().toString()))
                 .andExpect(jsonPath("$.id").value(mockResponse.id().toString()));
+    }
+
+    @Test
+    @WithMockUser
+    void testGenerateExerciseV2() throws Exception {
+        ExerciseResponse mockResponse = new ExerciseResponse(
+                UUID.fromString("123e4567-e89b-12d3-a456-426614174000"),
+                "1 + 1 = ?",
+                Status.CORRECT
+        );
+
+        when(generateExerciseService.generate()).thenReturn(mockResponse);
+
+        String expectedJson = objectMapper.writeValueAsString(mockResponse);
+
+        mockMvc.perform(get("/api/v7/exercise-generator").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(content().json(expectedJson));
     }
 }
